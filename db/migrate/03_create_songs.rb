5fcaa3a20db852589f700412a6c6c155a3eb1f43
Jason Whitlock <jrwhitlock7@gmail.com>
2021-04-16 15:09:22 -0700

    commit tree 398c638035e8b860986d2644c9e53a4948eac97f

--- db/migrate/03_create_songs.rb
+++ db/migrate/03_create_songs.rb
@@ -1,2 +1,11 @@
 class CreateSongs < ActiveRecord::Migration[4.2]
+    def change
+        create_table :songs do |t|
+            t.string :name
+            t.integer :artist_id
+            t.integer :genre_id
+        end
+    end
 end
+
+#whenever we create a song, itll have these as attributes^
